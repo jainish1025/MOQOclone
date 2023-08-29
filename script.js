efb6959b8650e8e2cda3280b0092515d4cde771a
[Jainish] <[jeinishgamit@gmail.com]>
2023-08-29 14:03:37 +0530

    [script.js]

--- script.js
+++ script.js
@@ -2,35 +2,35 @@ gsap;
 
 var tl = gsap.timeline();
 
-// function time() {
-//   var a = 0; setInterval(function(){
-//     a = a + Math.floor(Math.random()*15)
-//     if(a<100){
-//       document.querySelector("#loader h1").
-//       innerHTML = a+ "%"
-//     }
-//     else{
-//       a = 100
-//       document.querySelector("#loader h1").
-//       innerHTML = a+"%"
-//     }
-//   }, 150)
-// }
+function time() {
+  var a = 0; setInterval(function(){
+    a = a + Math.floor(Math.random()*15)
+    if(a<100){
+      document.querySelector("#loader h1").
+      innerHTML = a+ "%"
+    }
+    else{
+      a = 100
+      document.querySelector("#loader h1").
+      innerHTML = a+"%"
+    }
+  }, 150)
+}
 
-// time();
+time();
 
-// tl.to("#loader h1", {
-//   scale: 1.5,
-//   delay: 0.5,
-//   duration: 1,
-//   onStart:time()
-// });
+tl.to("#loader h1", {
+  scale: 1.5,
+  delay: 0.5,
+  duration: 1,
+  onStart:time()
+});
 
-// tl.to("#loader", {
-//   top: "-100vh",
-//   delay: 0.5,
-//   duration: 1.5,
-// });
+tl.to("#loader", {
+  top: "-100vh",
+  delay: 0.5,
+  duration: 1.5,
+});
 
 tl.from("#page1 #nav", {
   opacity: 0,
